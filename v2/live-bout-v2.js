3e786a47c9f941a0fff94a04288f8b7d93d5c8f9
matsidewrestlingco-netizen <matsidewrestlingco@gmail.com>
2025-12-16 10:56:55 -0500

    Update live-bout-v2.js

--- v2/live-bout-v2.js
+++ v2/live-bout-v2.js
@@ -1,3 +1,30 @@
+const BOUT_ID = 'hardcoded-for-now';
+
+async function fetchBout() {
+  // SELECT * FROM matside_v2.bouts WHERE id = BOUT_ID
+}
+
+function render(bout) {
+  renderHeader(bout);
+  renderStateBanner(bout);
+  renderActions(bout);
+}
+
+async function startMatch() {
+  await rpc('rpc_bout_start', { bout_id: BOUT_ID });
+  await refresh();
+}
+
+async function score(color, points) {
+  await rpc('rpc_apply_score_action', { /* explicit args */ });
+  await refresh();
+}
+
+async function refresh() {
+  const bout = await fetchBout();
+  render(bout);
+}
+
 console.log('live-bout-v2 loaded');
 
 document.getElementById('boutHeader').innerHTML = `
